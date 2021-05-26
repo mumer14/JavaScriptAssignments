2d88f87573e3bb2d8c2234f6720fdde40d972657
mumer14 <84837260+mumer14@users.noreply.github.com>
2021-05-26 12:55:40 +0500

    Update Task6.js

--- JS_Assignment2_PIAIC76820/Task6.js
+++ JS_Assignment2_PIAIC76820/Task6.js
@@ -1,4 +1,4 @@
-var Qualification= ['SSC', 'HSSC', 'BCS', 'BS', 'BCOM', 'MS', 'M.Phil', 'PhD']
+var Qualification= ['SSC', 'HSSC', 'BCS', 'BS', 'BSCS', 'MS', 'M.Phil', 'PhD']
 document.write ("Qualification : ")
 for(var i=1 ; i < 8; i++){
   document.write ("<br><br>"+ i + ') '+ Qualification[i - 1]);
